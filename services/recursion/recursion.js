26e7a86df31d6c63ebfdb12edaf0093466f96bac
israel litvak <lisrael1989@gmail.com>
2024-09-19 08:52:19 +0300

    add ex recursion

--- services/recursion/recursion.js
+++ services/recursion/recursion.js
@@ -43,6 +43,32 @@ console.log(
 );
 
 console.log('---------------------------- EX 4 Recursion-------------------------------------');
+// function sumArr(arr, sum, index) {
+//   if (arr.length === 0) {
+//     return sum / index;
+//   }
+//   const firstEl = arr.shift();
+//   sum += firstEl;
+//   index += 1;
+//   return sumArr(arr, sum, index);
+// }
+// console.log(sumArr([200, 150, 400, 100], 0, 0));
+
+/*
+1: arr [1,2,3]
+  sum = 0
+  idx = 0
+2: arr [2,3]
+  sum = 1
+  idx = 1
+3: arr [3]
+  sum = 3
+  idx = 2
+4 arr []
+  sum = 6
+  idx = 3
+
+*/
 
 function avgDistance(arr) {
   function sumArr(arr) {
@@ -63,3 +89,41 @@ function avgDistance(arr) {
 }
 
 console.log('🚀 ~  avgDistance(arr):', avgDistance([200, 150, 400, 100]));
+
+console.log('---------------------------- EX 5 Recursion-------------------------------------');
+
+function sumTripCosts(tripCosts) {
+  if (typeof tripCosts === 'number') {
+    return tripCosts;
+  }
+  return tripCosts.reduce((total, cost) => total + sumTripCosts(cost), 0);
+}
+
+console.log('🚀 ~  sumTripCosts(arr):', sumTripCosts([[100, 200], [150, 300], [50]]));
+
+console.log('---------------------------- EX 6 Recursion-------------------------------------');
+
+// function sumArr(arr, sum, index) {
+//   if (arr.length === 0) {
+//     return sum / index;
+//   }
+//   const firstEl = arr.shift();
+//   sum += firstEl;
+//   index += 1;
+//   return sumArr(arr, sum, index);
+// }
+// console.log(sumArr([200, 150, 400, 100], 0, 0));
+
+function bigNums(arr, big, index) {
+  if (arr.length === 0) {
+    return big;
+  }
+  const bigNum = arr.shift();
+  if (bigNum > big) {
+    big = bigNum;
+  }
+  index++;
+  return bigNums(arr, big, index);
+}
+
+console.log('the biggest number is:', bigNums([10, 20, 15, 25], 0, 0));
